dea193d24653a77d09e2ac0e6a05359be4c73bd2
Alexander Zhilyaev <azh@hellofresh.com>
2016-05-10 15:17:37 +0200

    Refactored code, added examples and refactored unit tests

--- crossengage/client.py
+++ crossengage/client.py
@@ -1,14 +1,15 @@
 import json
-
 import requests
+
+from requests.exceptions import RequestException
 
 
 class CrossengageClient(object):
     """
-    Client for Crossengage public API.
+    Client for Crossengage public API. Support create_user, update_user, delete_user API calls.
     usage:
-     import client
-     client = CrossengageClient(your_token)
+     from crossengage.client import CrossengageClient
+     client = CrossengageClient(client_token='Place your token here')
      try:
          r = client.create_user(payload={'here_payload_key': 'here_payload_value'})
          if r.success:
@@ -20,7 +21,13 @@ class CrossengageClient(object):
     """
     API_URL = 'https://api.crossengage.io'
 
-    USER_URL = '/users/'
+    API_VERSION = '1'
+
+    USER_ENDPOINT = '/users/'
+
+    REQUEST_PUT = 'put'
+
+    REQUEST_DELETE = 'delete'
 
     def __init__(self, client_token):
         self.client_token = client_token
@@ -29,53 +36,74 @@ class CrossengageClient(object):
         self.headers = {}
 
     def create_user(self, payload):
+        # type: (dict) -> dict
         """
         Create User given its id.
-        :param payload: dict of payload (email, id, firstName, lastName, birthday ..)
-        :return: json response, for example: {"id":"123", "xngGlobalUserId": "xng-id", "success": "true}
+        :param payload: dict of payload (email, id, firstName, lastName, birthday, createdAt, gender)
+        :return: json dict response, for example: {"status_code": 200, "id":"123", "xngGlobalUserId": "xng-id",
+         "success": "true}
         """
-        return self.__sync_user(payload, put_type=True)
+        return self.__sync_user(payload=payload, request_type=self.REQUEST_PUT)
 
     def update_user(self, payload):
+        # type: (dict) -> dict
         """
         Update User given its id.
-        :param payload: dict of payload (email, id, firstName, lastName, birthday ..)
-        :return: json response, for example: {"id":"123", "xngGlobalUserId": "xng-id", "success": "true}
+        :param payload: dict of payload (email, id, firstName, lastName, birthday, createdAt, gender)
+        :return: json dict response, for example: {"status_code": 200, "id":"123", "xngGlobalUserId": "xng-id",
+         "success": "true}
         """
-        return self.__sync_user(payload, put_type=True)
+        return self.__sync_user(payload=payload, request_type=self.REQUEST_PUT)
 
     def delete_user(self, payload):
+        # type: (dict) -> dict
         """
         Delete User given its id.
-        :param payload: dict of payload (id only)
-        :return: status code
+        :param payload: dict of payload (id)
+        :return: json dict response, for example: {"status_code": 200}
         """
-        return self.__sync_user(payload, put_type=False)
+        return self.__sync_user(payload=payload, request_type=self.REQUEST_DELETE)
 
-    def __sync_user(self, payload, put_type):
-        self.request_url = self.API_URL + self.USER_URL + payload['id']
-        response = self.__create_request(payload, put_type)
-        return response
+    def __sync_user(self, payload, request_type):
+        if 'id' in payload:
+            self.request_url = self.API_URL + self.USER_ENDPOINT + payload['id']
+            return self.__create_request(payload, request_type)
 
-    def __create_request(self, payload, put_type=True):
+        return {'success': False, 'client_error': 'Missing id in payload', 'errors': ''}
+
+    def __create_request(self, payload, request_type):
         self.headers = {
             'X-XNG-AuthToken': self.client_token,
-            'X-XNG-ApiVersion': '1',
+            'X-XNG-ApiVersion': self.API_VERSION,
             'Content-Type': 'application/json',
         }
 
-        if put_type:
-            r = self.requests.put(
-                self.request_url,
-                data=json.dumps(payload),
-                headers=self.headers
-            )
-            return r.json()
+        response = {}
 
-        r = self.requests.delete(
-            self.request_url,
-            data=json.dumps(payload),
-            headers=self.headers
-        )
+        try:
+            if request_type == self.REQUEST_PUT:
+                r = self.requests.put(self.request_url, data=json.dumps(payload), headers=self.headers)
+                response = r.json()
+            else:
+                r = self.requests.delete(self.request_url, data=json.dumps(payload), headers=self.headers)
+                if r.status_code == 204:
+                    response['success'] = True
+            response['status_code'] = r.status_code
 
-        return r.status_code
+        except RequestException as e:
+            # handle all requests HTTP exceptions
+            response = {'client_error': e.message}
+        except Exception as e:
+            # handle all exceptions which can be on API side
+            response = {'client_error': (e.message + '. Response: ' + r.text)}
+
+        if 'errors' not in response:
+            response['errors'] = ''
+        if 'status_code' not in response:
+            response['status_code'] = 0
+        if 'success' not in response:
+            response['success'] = False
+        if 'client_error' not in response:
+            response['client_error'] = ''
+
+        return response
--- tests/test_client.py
+++ tests/test_client.py
@@ -1,22 +1,43 @@
 import unittest
 
 from mock import Mock
+from requests import RequestException
 
 from crossengage.client import CrossengageClient
+
+
+class DummyRequest(object):
+    def __init__(self):
+        self.status_code = 200
+        self.text = 'Some text'
+
+    def put(self, request_url, data, headers):
+        return self
+
+    def delete(self, request_url, data, headers):
+        return self
+
+    @staticmethod
+    def json():
+        return {'success': True, 'errors': ''}
+
+
+class DummyRequestException(object):
+    def __init__(self):
+        self.status_code = 200
+
+    def put(self, request_url, data, headers):
+        raise RequestException('Something went wrong')
+
+    @staticmethod
+    def json():
+        return {'success': True, 'errors': ''}
 
 
 class TestCrossengageClient(unittest.TestCase):
     def setUp(self):
         self.client = CrossengageClient(client_token='SOME_TOKEN')
-
-    def test_init(self):
-        self.assertEqual(self.client.client_token, 'SOME_TOKEN')
-        self.assertIsNotNone(self.client.requests)
-        self.assertEqual(self.client.request_url, '')
-        self.assertEqual(self.client.headers, {})
-
-    def test_create_user(self):
-        payload = {
+        self.payload = {
             'email': 'email@example.com',
             'id': '1234',
             'firstName': 'Firstname',
@@ -25,40 +46,54 @@ class TestCrossengageClient(unittest.TestCase):
             'createdAt': '2015-10-02T08:23:53Z',
             'gender': 'male',
         }
-        self.client.requests = Mock()
-        self.client.requests.put.json.return_value = {'success': 'true'}
-        self.client.create_user(payload=payload)
+
+    def test_init(self):
+        self.assertEqual(self.client.client_token, 'SOME_TOKEN')
+        self.assertIsNotNone(self.client.requests)
+        self.assertEqual(self.client.request_url, '')
+        self.assertEqual(self.client.headers, {})
+
+    def test_create_update_user(self):
+        self.client.requests = DummyRequest()
+        response = self.client.create_user(payload=self.payload)
 
         self.assertEqual('https://api.crossengage.io/users/1234', self.client.request_url)
         self.assertEqual(self.client.headers['X-XNG-AuthToken'], 'SOME_TOKEN')
         self.assertEqual(self.client.headers['Content-Type'], 'application/json')
 
-    def test_update_user(self):
-        payload = {
-            'email': 'email@example.com',
-            'id': '1235',
-            'firstName': 'Firstname2',
-            'lastName': 'Lastname2',
-            'birthday': '1991-11-07',
-            'createdAt': '2015-10-02T08:23:53Z',
-            'gender': 'male',
-        }
-        self.client.requests = Mock()
-        self.client.requests.put.json.return_value = {'success': 'true'}
-        self.client.create_user(payload=payload)
+        self.assertEqual(response['status_code'], 200)
+        self.assertEqual(response['errors'], '')
+        self.assertEqual(response['client_error'], '')
+        self.assertEqual(response['success'], True)
 
-        self.assertEqual('https://api.crossengage.io/users/1235', self.client.request_url)
+    def test_create_update_user_request_exception(self):
+        self.client.requests = DummyRequestException()
+        response = self.client.create_user(payload=self.payload)
+
+        self.assertEqual('https://api.crossengage.io/users/1234', self.client.request_url)
         self.assertEqual(self.client.headers['X-XNG-AuthToken'], 'SOME_TOKEN')
         self.assertEqual(self.client.headers['Content-Type'], 'application/json')
+
+        self.assertEqual(response['status_code'], 0)
+        self.assertEqual(response['errors'], '')
+        self.assertEqual(response['client_error'], 'Something went wrong')
+        self.assertEqual(response['success'], False)
 
     def test_delete_user(self):
         payload = {
-            'id': '1236',
+            'id': '1',
         }
-        self.client.requests = Mock(status_code=204)
-        status_code = self.client.delete_user(payload=payload)
+        dummy_request = DummyRequest()
+        dummy_request.status_code = 204
+        self.client.requests = dummy_request
+        response = self.client.delete_user(payload)
 
-        self.assertEqual('https://api.crossengage.io/users/1236', self.client.request_url)
+        self.assertEqual('https://api.crossengage.io/users/1', self.client.request_url)
         self.assertEqual(self.client.headers['X-XNG-AuthToken'], 'SOME_TOKEN')
         self.assertEqual(self.client.headers['X-XNG-ApiVersion'], '1')
         self.assertEqual(self.client.headers['Content-Type'], 'application/json')
+
+        self.assertEqual(response['status_code'], 204)
+        self.assertEqual(response['errors'], '')
+        self.assertEqual(response['client_error'], '')
+        self.assertEqual(response['success'], True)
